65d49f807bd54edb57274e73578dca6614b1c6c4
Marco Pettorali <marcopettorali1@gmail.com>
2025-07-31 18:21:03 +0200

    urbsense scenario solved

--- resourceallocation/urbsense/urbsense.py
+++ resourceallocation/urbsense/urbsense.py
@@ -19,10 +19,15 @@ def compute_coverage_matrix(*args, **kwargs):
 
 
 def qtime(gamma_exe: Distribution, process: Process, host: Host, cache_index=None, period=None):
-    # ASSUMPTION: gamma_exe is constant, I tale the median only
+    # ASSUMPTION: gamma_exe is constant, I take the median only
     gamma_exe_val = gamma_exe.percentile(50)
 
     # ASSUMPTION: all packets arrive at the same time
     # hence the queuing time is just the execution time * process.mns - 1 (the first packet does not wait)
-    queuing_time = Distribution.dirac_delta(gamma_exe_val * (process.mns - 1))
-    return queuing_time
+    queuing_time = gamma_exe_val * (process.mns - 1)
+
+    # If queuing_time is >= period, the system is not stable
+    if period is not None and queuing_time >= period:
+        queuing_time = 2000
+
+    return Distribution.dirac_delta(queuing_time)
