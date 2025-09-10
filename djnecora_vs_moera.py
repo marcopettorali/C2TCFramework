df34584dea697ee5a83fbe383b2d7b86d037b891
Marco Pettorali <marcopettorali1@gmail.com>
2025-09-10 12:09:04 +0200

    rollback to sequential order of experiments, parallel crashed

--- djnecora_vs_moera.py
+++ djnecora_vs_moera.py
@@ -6,7 +6,7 @@ import random
 from utils.logging import focus, set_logging_level
 
 SCENARIO = "scenario1"
-NUM_REPETITIONS = 10  # 50
+NUM_REPETITIONS = 50
 MAX_MNS = 13  # set to -1 to allocate all MNs of each process
 
 context = DJNecora.load_context_from_file(f"configs/{SCENARIO}.json")
@@ -16,73 +16,6 @@ selection_policies = ["first_fit", "next_fit", "best_fit", "worst_fit", "random_
 
 results_file = f"out/djnecora_vs_moera_{SCENARIO}.json"
 
-set_logging_level("focus")
-
-
-def _worker(rep_index):
-    focus(f"--- REPETITION {rep_index + 1}/{NUM_REPETITIONS} ---")
-
-    # Initialize algorithms
-    moera = MOERA()
-    moera.set_context(copy.deepcopy(context))
-
-    djnecora_dict = {sp: {cp: DJNecora(sp, cp) for cp in selection_policies} for sp in splitting_policies}
-    for sp in splitting_policies:
-        for cp in selection_policies:
-            djnecora_dict[sp][cp].set_context(copy.deepcopy(context))
-            for process_name, process in context.processes.items():
-                djnecora_dict[sp][cp].context.processes[process_name].mns = 1  # reset number of MNs to 1
-            djnecora_dict[sp][cp].initialize_hosts()
-
-    # get process list
-    process_list = list(context.processes.keys())
-
-    # get MNs list
-    mns_list = [p for p in process_list for _ in range(0, MAX_MNS if MAX_MNS >= 0 else context.processes[p].mns)]
-
-    # shuffle process list
-    random.shuffle(mns_list)
-
-    # transform each entry of the shuffled list in (process, i), where i is the index of the MN relative to process from 0 to N
-    # e.g. [P0, P1, P0, P2, P1] -> [(P0,0), (P1,0), (P0,1), (P2,0), (P1,1)]
-    mns_list = [(p, sum(1 for x in mns_list[:i] if x == p)) for i, p in enumerate(mns_list)]
-
-    # Keep track of allocation status for DJNecora (stop allocating MNs of a process if one MN could not be allocated)
-    allocation_status = {sp: {cp: {} for cp in selection_policies} for sp in splitting_policies}
-
-    # Extract one element at a time and allocate it using all the algorithms
-    for process_name, mn_index in mns_list:
-        focus(f"\tAllocating MN {mn_index} of process {process_name}")
-
-        # MOERA
-        moera.add_1_mn(process_name)
-
-        # DJNecora
-        for sp in splitting_policies:
-            for cp in selection_policies:
-                # if first mn of the process, call add_process, else call add_1_mn
-                if mn_index == 0:
-                    ret = djnecora_dict[sp][cp].add_process(process_name)
-                    allocation_status[sp][cp][process_name] = ret
-                    focus(f"\t\tDJNecora ({sp}, {cp}): adding process {process_name}: **{'succeeded' if ret else 'failed'}**")
-                else:
-                    if allocation_status[sp][cp][process_name]:
-                        ret = djnecora_dict[sp][cp].add_1_mn_to_process(process_name)
-                        allocation_status[sp][cp][process_name] = ret
-                        focus(f"\t\tDJNecora ({sp}, {cp}): adding 1 MN to process {process_name}: **{'succeeded' if ret else 'failed'}**")
-                    else:
-                        focus(
-                            f"\t\tDJNecora ({sp}, {cp}): skipping allocation of MN {mn_index} of process {process_name} since previous MNs could not be allocated"
-                        )
-
-    # store results
-    ret_dict = {}
-    ret_dict["MOERA"] = moera._allocation_map
-    for sp in splitting_policies:
-        for cp in selection_policies:
-            ret_dict[f"DJ-NECORA.{sp}.{cp}"] = djnecora_dict[sp][cp]._allocation_table_per_host
-    return ret_dict
-
 
 def run_experiments():
 
@@ -91,20 +24,71 @@ def run_experiments():
     results = {"MOERA": []}
     results.update({f"DJ-NECORA.{sp}.{cp}": [] for sp in splitting_policies for cp in selection_policies})
 
-    import multiprocessing as mp
+    for rep in range(NUM_REPETITIONS):
+        focus(f"--- REPETITION {rep + 1}/{NUM_REPETITIONS} ---")
 
-    # collect results from multiple processes
-    with mp.Pool(processes=mp.cpu_count()) as pool:
-        data = pool.map(_worker, range(NUM_REPETITIONS))
+        # Initialize algorithms
+        moera = MOERA()
+        moera.set_context(copy.deepcopy(context))
 
-    focus("All repetitions completed")
+        djnecora_dict = {sp: {cp: DJNecora(sp, cp) for cp in selection_policies} for sp in splitting_policies}
+        for sp in splitting_policies:
+            for cp in selection_policies:
+                djnecora_dict[sp][cp].set_context(copy.deepcopy(context))
+                for process_name, process in context.processes.items():
+                    djnecora_dict[sp][cp].context.processes[process_name].mns = 1  # reset number of MNs to 1
+                djnecora_dict[sp][cp].initialize_hosts()
 
-    # process results
-    for rep in data:
-        for key, value in rep.items():
-            results[key].append(value)
+        set_logging_level("focus")
 
-    focus("Results processed")
+        # get process list
+        process_list = list(moera.context.processes.keys())
+
+        # get MNs list
+        mns_list = [p for p in process_list for _ in range(0, MAX_MNS if MAX_MNS >= 0 else context.processes[p].mns)]
+
+        # shuffle process list
+        random.shuffle(mns_list)
+
+        # transform each entry of the shuffled list in (process, i), where i is the index of the MN relative to process from 0 to N
+        # e.g. [P0, P1, P0, P2, P1] -> [(P0,0), (P1,0), (P0,1), (P2,0), (P1,1)]
+        mns_list = [(p, sum(1 for x in mns_list[:i] if x == p)) for i, p in enumerate(mns_list)]
+
+        # Keep track of allocation status for DJNecora (stop allocating MNs of a process if one MN could not be allocated)
+        allocation_status = {sp: {cp: {} for cp in selection_policies} for sp in splitting_policies}
+
+        # Extract one element at a time and allocate it using all the algorithms
+        for process_name, mn_index in mns_list:
+            focus(f"\tAllocating MN {mn_index} of process {process_name}")
+
+            # MOERA
+            moera.add_1_mn(process_name)
+
+            # DJNecora
+            for sp in splitting_policies:
+                for cp in selection_policies:
+                    # if first mn of the process, call add_process, else call add_1_mn
+                    if mn_index == 0:
+                        ret = djnecora_dict[sp][cp].add_process(process_name)
+                        allocation_status[sp][cp][process_name] = ret
+                        focus(f"\t\tDJNecora ({sp}, {cp}): adding process {process_name}: **{'succeeded' if ret else 'failed'}**")
+                    else:
+                        if allocation_status[sp][cp][process_name]:
+                            ret = djnecora_dict[sp][cp].add_1_mn_to_process(process_name)
+                            allocation_status[sp][cp][process_name] = ret
+                            focus(
+                                f"\t\tDJNecora ({sp}, {cp}): adding 1 MN to process {process_name}: **{'succeeded' if ret else 'failed'}**"
+                            )
+                        else:
+                            focus(
+                                f"\t\tDJNecora ({sp}, {cp}): skipping allocation of MN {mn_index} of process {process_name} since previous MNs could not be allocated"
+                            )
+
+        # store results
+        results["MOERA"].append(moera._allocation_map)
+        for sp in splitting_policies:
+            for cp in selection_policies:
+                results[f"DJ-NECORA.{sp}.{cp}"].append(djnecora_dict[sp][cp]._allocation_table_per_host)
 
     # dump data to json
     import json
