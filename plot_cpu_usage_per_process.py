9fd07c080be7afb6c7b5c2d4f8de26a92d2c1158
Marco Pettorali <marcopettorali1@gmail.com>
2025-09-10 11:36:42 +0200

    parallel execution of experiments

--- plot_cpu_usage_per_process.py
+++ plot_cpu_usage_per_process.py
@@ -11,6 +11,8 @@ host: Host = context.hosts["BR0"]
 
 process: Process
 for process_name, process in context.processes.items():
+    if process_name in ["P6", "P7"]:  # skip processes with very high CPU requirements
+        continue
     max_delay_ms = process.max_delay_ms
 
     # for (p, h, c, m), g in context.links["gamma_tot_precomputed"].items():
@@ -19,7 +21,12 @@ for process_name, process in context.processes.items():
     #         print(f"Process: {p}, Host: {h}, CPU Share: {c:<.2f}, CPU GHz: {c * host.cpu_ghz:<.2f}, MNs: {m}, Delay: {g:<.2f}")
     # exit()
 
-    for mns in range(1, MAX_MNS + 1):
+    for mns in range(0, MAX_MNS + 1):
+
+        if mns == 0:
+            processes_data[process_name] = processes_data.get(process_name, {})
+            processes_data[process_name][mns] = 0.0
+            continue
 
         # find min CPU share to satisfy those MNs
         best_fit_record = min(
@@ -36,4 +43,23 @@ for process_name, process in context.processes.items():
         processes_data[process_name] = processes_data.get(process_name, {})
         processes_data[process_name][mns] = cpu_share
 
-print(processes_data)
+
+# plot one curve per process
+import matplotlib.pyplot as plt
+from utils.plotting import latex_initialize, bold
+latex_initialize()
+
+colors = ["tab:red", "tab:blue", "tab:green",  "tab:purple", "tab:orange","tab:olive"]
+markers = ['x', 'o', 's', 'd', 'v', '^']
+for process_name, mns_data in processes_data.items():
+    mns_values = list(mns_data.keys())
+    cpu_share_values = list(mns_data.values())
+    plt.plot(mns_values, cpu_share_values, label=bold(process_name), color=colors.pop(0), marker=markers.pop(0))
+
+plt.xlim(0,13)
+plt.ylim(0,6)
+plt.xlabel(bold("Number of MNs"))
+plt.ylabel(bold("CPU usage (GHz)"))
+plt.legend(ncols=3)
+plt.grid()
+plt.show()
--- resourceallocation/djnecora.py
+++ resourceallocation/djnecora.py
@@ -40,7 +40,7 @@ class DJNecora(JNecora):
         pickle_context: bool = True,
         pickle_folder_relative_path: str = "pickles/djnecora",
         # _cpu_shares=list([float(x) for x in np.linspace(0, 1, 51)][1:]),
-        _cpu_shares={"cpu_ghz_precision": 0.1},  # 100 MHz precision
+        _cpu_shares={"cpu_ghz_precision": 0.01},  # 10 MHz precision
     ):
         return JNecora.load_context_from_file(config_path, pickle_context, pickle_folder_relative_path, _cpu_shares=_cpu_shares)
 
@@ -263,6 +263,7 @@ class DJNecora(JNecora):
 
         candidates = self._prefer_infinite_parallelism(candidates)
         debug(f"Candidates after preferring infinite parallelism: {candidates}")
+        
         if not candidates:
             info(f"No candidate hosts available for adding 1 MN to process {process_name}")
             return False
